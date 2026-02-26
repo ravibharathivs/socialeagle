d6b6fb028e6331caa75a4421563791c60317199e
ravibharathivs <ravibharathivs@gmail.com>
2026-02-26 08:21:52 +0530

    Week1 Day3 assignment code comited

---
+++ Week1/Day3/SimpleCal.py
@@ -0,0 +1,14 @@
+def calc(num1,num2,operator):
+    if(operator=='+'):
+        return num1+num2
+    elif(operator=='-'):
+        return num1-num2
+    elif(operator=='*'):
+        return num1*num2
+    elif(operator=='/'):
+        return num1/num2
+    
+num1 = int(input("Enter the num1 : "))
+num2 = int(input("Enter the num2 : "))
+operator = input("Enter the operator ( + - * / ) to perform the operation : ")
+print(calc(num1,num2,operator))
